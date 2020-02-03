b75fa85daa85483aad94ce79c72750ac6fe4d910
marciovz <marciovz@yahoo.com.br>
2020-02-03 19:02:59 -0300

    Implementado migração ,model, controller, e rota para casdastro de um destinatário (recipient)

---
+++ backend/src/app/controllers/RecipientController.js
@@ -0,0 +1,27 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      complement: Yup.string().required(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fail' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+    console.log(req.recipient);
+
+    return res.json(recipient);
+  }
+}
+
+export default new RecipientController();
