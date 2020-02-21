d5f187214498c85f84b1e7cedca831932fe3059e
marciovz <marciovz@yahoo.com.br>
2020-02-21 15:03:01 -0300

    Implementado roteamento de rotas autenticadas

---
+++ web/src/routes/Route.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+// import AuthLayout from '~/pages/_layouts/auth';
+// import DefaultLayout from '~/pages/_layouts/default';
+
+// import { store } from '~/store';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  //  const { signed } = store.getState().auth;
+  const signed = true;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/delivery" />;
+  }
+
+  // const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
 import Delivery from '~/pages/Delivery';
@@ -11,10 +12,10 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/delivery" component={Delivery} />
-      <Route path="/deliveryman" component={Deliveryman} />
-      <Route path="/recipient" component={Recipient} />
-      <Route path="/problem" component={Problem} />
+      <Route path="/delivery" component={Delivery} isPrivate />
+      <Route path="/deliveryman" component={Deliveryman} isPrivate />
+      <Route path="/recipient" component={Recipient} isPrivate />
+      <Route path="/problem" component={Problem} isPrivate />
     </Switch>
   );
 }
