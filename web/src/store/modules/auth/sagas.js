a9a5ae45bbe7f9a2b3b2cf11e23bd7141ce3e8ef
marciovz <marciovz@yahoo.com.br>
2020-02-25 09:59:41 -0300

    Implementado mensagem de erro na autenticação com Toastify

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -19,6 +20,7 @@ export function* signIn({ payload }) {
 
     history.push('/delivery');
   } catch (error) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
