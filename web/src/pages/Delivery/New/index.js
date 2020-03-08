99222394eff6dfd63adb8d588ff15c816bb11905
marciovz <marciovz@yahoo.com.br>
2020-03-08 18:58:50 -0300

    Implementado a pagina de cadastro de entregas

--- web/src/pages/Delivery/New/index.js
+++ web/src/pages/Delivery/New/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+
+import history from '~/services/history';
+import api from '~/services/api';
 
 import HeaderPagesForm from '~/components/HeaderPagesdForm';
 import ButtonSave from '~/components/Form/Buttons/Save';
@@ -9,14 +13,33 @@ import FormDelivery from '~/components/Form/FormDelivery';
 import { Container } from './styles';
 
 export default function New() {
+  function handleGoBack() {
+    history.push('/delivery');
+  }
+
+  async function handleSave({ recipient, deliveryman, product }) {
+    try {
+      await api.post('/deliveries', {
+        recipient_id: recipient.value,
+        deliveryman_id: deliveryman.value,
+        product,
+      });
+      toast.success('Entrega cadastrada com sucesso!');
+      history.push('/delivery');
+    } catch (err) {
+      console.tron.log(err);
+      toast.error('Não foi possível cadastrar a entrega');
+    }
+  }
+
   return (
     <Container>
       <HeaderPagesForm title="Cadastro de encomendas">
-        <ButtonGoBack />
-        <ButtonSave />
+        <ButtonGoBack onClick={handleGoBack} />
+        <ButtonSave type="submit" form="formDelivery" />
       </HeaderPagesForm>
       <DashboardForm>
-        <FormDelivery />
+        <FormDelivery onSubmit={handleSave} />
       </DashboardForm>
     </Container>
   );
