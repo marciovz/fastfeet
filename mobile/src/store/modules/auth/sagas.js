d7658b5c3de95d52d3946005606059186422c9f1
marciovz <marciovz@yahoo.com.br>
2020-03-20 20:54:38 -0300

    Implementado redux-saga na aplicação

---
+++ mobile/src/store/modules/auth/sagas.js
@@ -0,0 +1,44 @@
+import {Alert} from 'react-native';
+import {takeLatest, call, put, all} from 'redux-saga/effects';
+
+import api from '~/services/api';
+
+import {signInSuccess, signFailure} from './actions';
+
+export function* signIn({payload}) {
+  try {
+    const {deliverymanId} = payload;
+
+    const response = yield call(api.post, 'sessionsDeliveryman', {
+      id: deliverymanId,
+    });
+
+    const deliveryman = response.data;
+
+    if (!deliveryman) {
+      Alert.alert(
+        'Error no login',
+        'Entregador não encontado, verifique seu id',
+      );
+      return;
+    }
+
+    yield put(signInSuccess(deliveryman));
+  } catch (err) {
+    Alert.alert(
+      'Falha na autenticação',
+      'Houve um erro no login, verifique seus dados',
+    );
+    yield put(signFailure());
+  }
+}
+
+export function setProfile({payload}) {
+  console.tron.log('payload - Rehydrate');
+  console.tron.log(payload);
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setProfile),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
